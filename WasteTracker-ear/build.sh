f1e84f6b5caa8ac1bef79b949cb394913f8c08eb
Marc-André Laverdière <marc-andre@atc.tcs.com>
2014-05-01 22:01:16 -0400

    Disabled offline mode and local copy

--- WasteTracker-ear/build.sh
+++ WasteTracker-ear/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 cd ../resteasy
-mvn -o clean install
+mvn clean install
 if [ $? = 1 ]; then
         exit 1
 fi
 
 cd ../WasteTracker-Server
-mvn -o clean install -P deploy
+mvn clean install -P deploy
 if [ $? = 1 ]; then
         exit 1
 fi
 
 cd ../WasteTracker-ear
-mvn -o clean package
+mvn clean package
 
-cp target/WasteTracker.ear /home/fidox/Projects/WasteTracker/deploy
+#cp target/WasteTracker.ear /home/fidox/Projects/WasteTracker/deploy
